c4997bf681cca47322741d24c48c84f84b10fd6e
Aarya1929 <nirgudeaarya123@gmail.com>
2024-06-12 12:56:18 +0530

    java&dsa

---
+++ pattern questions/half_pyr_numbers.java
@@ -0,0 +1,18 @@
+/*Half pyramid of numbers:
+1
+12
+123
+1234
+12345 
+ */
+
+public class half_pyr_numbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
