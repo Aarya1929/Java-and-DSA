d290a525ec1aa2a7e43384af885c74ee5dd0ef5f
Aarya1929 <nirgudeaarya123@gmail.com>
2024-06-12 15:15:43 +0530

    next

--- pattern questions/butterfly.java
+++ pattern questions/butterfly.java
@@ -1,5 +1,50 @@
+/* Butterfly pattern:
+ *      *
+ **    **
+ ***  ***
+ ********
+ ********
+ ***  ***
+ **    **
+ *      *
+ */
+
 public class butterfly {
     public static void main(String[] args) {
-        
+        int n = 4; // Number of rows for one half of the butterfly
+
+        // Upper part of the butterfly
+        for (int i = 1; i <= n; i++) {
+            // Print left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Print spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // Print right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower part of the butterfly
+        for (int i = n; i >= 1; i--) {
+            // Print left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Print spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // Print right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ pattern questions/solid_rhombus.java
@@ -0,0 +1,24 @@
+/*Solid rhombus:
+    *****
+   *****
+  *****
+ ***** 
+***** 
+ */
+
+public class solid_rhombus {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+    }
+}
